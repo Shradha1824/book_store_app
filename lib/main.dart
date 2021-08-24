58cd2a593f1afb351c09db931a00a9498c8a0409
Shradha1824 <shradhabj1824@gmail.com>
2021-08-24 08:20:35 -1200

    My First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white10, // navigation bar color
+    statusBarColor: Colors.white10,
+    statusBarBrightness: Brightness.light, // status bar color
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MaterialApp(),
+    );
+  }
+}
