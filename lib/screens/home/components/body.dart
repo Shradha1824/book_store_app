a5d815cd125cc552366e656c13f62e5b7489d41e
Shradha1824 <shradhabj1824@gmail.com>
2021-08-26 19:34:25 -1200

    This folder is created to create gridview and to make Good UI

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,101 @@
+import 'package:book_store_app/constants.dart';
+import 'package:book_store_app/models/books.dart';
+import 'package:book_store_app/screens/home/components/item_card.dart';
+import 'package:book_store_app/screens/single_book_details_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(padding: EdgeInsets.all(10)),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text("Books",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              )),
+          dropDown(),
+        ]),
+        SizedBox(
+          height: kDefaultPadding,
+        ),
+        Expanded(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: GridView.builder(
+                    itemCount: books.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: MediaQuery.of(context).orientation ==
+                                Orientation.landscape
+                            ? 3
+                            : 2,
+                        //crossAxisCount: 2,
+                        mainAxisSpacing: kDefaultPadding / 1.5,
+                        crossAxisSpacing: kDefaultPadding / 1.5,
+                        // crossAxisCount: orientation == Orientation.portrait ? 2 : 5,
+                        childAspectRatio: (110.0 / 220.0)),
+                    itemBuilder: (context, index) => ItemCard(
+                          books: books[index],
+                          press: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SingleBookPage(
+                                        books: books[index],
+                                      ))),
+                        )))),
+      ],
+    );
+  }
+}
+
+class dropDown extends StatefulWidget {
+  @override
+  dropDownState createState() => dropDownState();
+}
+
+class dropDownState extends State<dropDown> {
+  List<String> _relevence = [
+    ' Price: Low to High',
+    ' Price: High to Low',
+    ' Newest Arrivals',
+  ]; // Option 2
+  String? _selectedLocation;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 140,
+        height: 30,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+        ),
+        child: DropdownButton(
+          iconSize: 25,
+          underline: SizedBox(),
+          hint: Text(
+            ' Sort by relevance   ',
+            style: TextStyle(fontSize: 12),
+          ), // Not necessary for Option 1
+          value: _selectedLocation,
+          onChanged: (String? newValue) {
+            setState(() {
+              _selectedLocation = newValue;
+            });
+          },
+          items: _relevence.map((relevence) {
+            return DropdownMenuItem(
+              child: Text(
+                relevence,
+                style: TextStyle(fontSize: 12),
+                textAlign: TextAlign.left,
+              ),
+              value: relevence,
+            );
+          }).toList(),
+        ));
+  }
+}
---
+++ lib/screens/home/components/item_card.dart
@@ -0,0 +1,59 @@
+import 'package:book_store_app/constants.dart';
+import 'package:book_store_app/models/books.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ItemCard extends StatelessWidget {
+  final Books books;
+  final Function press;
+  const ItemCard({
+    Key? key,
+    required this.books,
+    required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        // onTap: press(),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      Expanded(
+          child: Container(
+        padding: EdgeInsets.all(kDefaultPadding),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.grey.shade300)),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(books.image),
+            SizedBox(
+              height: 2,
+            ),
+            Text(books.title),
+            SizedBox(
+              height: 2,
+            ),
+            Text("by ${books.author}"),
+            SizedBox(
+              height: 2,
+            ),
+            Text("Rs. ${books.price}"),
+            SizedBox(
+              height: 2,
+            ),
+            RaisedButton(
+                child: Text(
+                  "ADD TO CARD",
+                  style: TextStyle(fontSize: 10),
+                ),
+                onPressed: () {})
+          ],
+        ),
+      ))
+    ]));
+  }
+}
