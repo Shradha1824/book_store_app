5fa0022800107850fee60e49b7f446174d432b21
Shradha1824 <shradhabj1824@gmail.com>
2021-08-27 00:02:17 -1200

    Display each book details

--- lib/screens/single_book_details_screen.dart
+++ lib/screens/single_book_details_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:book_store_app/constants.dart';
 import 'package:book_store_app/models/books.dart';
+import 'package:book_store_app/screens/details/components/card_counter.dart';
 import 'package:flutter/material.dart';
 
 class SingleBookPage extends StatelessWidget {
@@ -13,12 +15,11 @@ class SingleBookPage extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.orangeAccent,
         ),
-        body: Column(children: [
+        body: SingleChildScrollView(
+            child: Column(children: [
           //Text(image),
           Container(
               margin: EdgeInsets.only(top: 50, right: 15, left: 15, bottom: 15),
-              width: 400,
-              height: 250,
               //color: Colors.blue,
               padding: EdgeInsets.only(left: 10, bottom: 15, right: 15),
               decoration: BoxDecoration(
@@ -32,14 +33,38 @@ class SingleBookPage extends StatelessWidget {
                         // mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Container(
-                            height: 170,
+                            height: 220,
                             width: 170,
-                            // child: Image.asset(image),
+                            child: Image.asset(
+                              books.image,
+                              fit: BoxFit.fill,
+                            ),
                           ),
-                          //Text(title),
+                          SizedBox(
+                            width: kDefaultPadding,
+                          ),
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: kDefaultPadding,
+                                ),
+                                Text(books.title),
+                                SizedBox(
+                                  height: kDefaultPadding,
+                                ),
+                                Text(books.author),
+                                SizedBox(
+                                  height: kDefaultPadding,
+                                ),
+                                Text("Rs. ${books.price}"),
+                                SizedBox(
+                                  height: kDefaultPadding,
+                                ),
+                                CardCounter()
+                                //Text(title),
+                              ])
                         ]),
-                    //Text(author),
-                    //Text(price),
                     RaisedButton(
                       onPressed: () {},
                       child: Text("PLACE ORDER",
@@ -47,32 +72,30 @@ class SingleBookPage extends StatelessWidget {
                             color: Colors.white,
                           )),
                       color: Colors.orangeAccent,
-                    ),
+                    )
                   ])),
-          Container(
-            margin: EdgeInsets.only(left: 15, right: 15, bottom: 15),
-            padding: EdgeInsets.all(20),
-            width: 400,
-            height: 60,
-            child: Text(
-              "Customer Details",
-              style: TextStyle(color: Colors.black.withOpacity(0.8)),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              margin: EdgeInsets.only(top: 15, right: 15, left: 15, bottom: 15),
+              padding: EdgeInsets.only(left: 10, bottom: 15, right: 15),
+              child: Text(
+                "Customer Details",
+                style: TextStyle(color: Colors.black.withOpacity(0.8)),
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade400)),
             ),
-            decoration:
-                BoxDecoration(border: Border.all(color: Colors.grey.shade400)),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 15, right: 15, bottom: 15),
-            padding: EdgeInsets.all(20),
-            width: 400,
-            height: 60,
-            child: Text(
-              "Order Summery",
-              style: TextStyle(color: Colors.black.withOpacity(0.8)),
-            ),
-            decoration:
-                BoxDecoration(border: Border.all(color: Colors.grey.shade400)),
-          )
-        ]));
+            Container(
+              margin: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Order Summery",
+                style: TextStyle(color: Colors.black.withOpacity(0.8)),
+              ),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade400)),
+            )
+          ])
+        ])));
   }
 }
