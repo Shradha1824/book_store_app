130534eaeb0dfee6ae9eb77f950f816f508c99c0
Shradha1824 <shradhabj1824@gmail.com>
2021-08-27 00:09:53 -1200

    Modified some images

--- lib/screens/home/components/item_card.dart
+++ lib/screens/home/components/item_card.dart
@@ -5,7 +5,7 @@ import 'package:flutter/widgets.dart';
 
 class ItemCard extends StatelessWidget {
   final Books books;
-  final Function press;
+  final press;
   const ItemCard({
     Key? key,
     required this.books,
@@ -15,45 +15,58 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        // onTap: press(),
+        onTap: press,
         child:
             Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-      Expanded(
-          child: Container(
-        padding: EdgeInsets.all(kDefaultPadding),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: Colors.grey.shade300)),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(books.image),
-            SizedBox(
-              height: 2,
-            ),
-            Text(books.title),
-            SizedBox(
-              height: 2,
-            ),
-            Text("by ${books.author}"),
-            SizedBox(
-              height: 2,
-            ),
-            Text("Rs. ${books.price}"),
-            SizedBox(
-              height: 2,
-            ),
-            RaisedButton(
-                child: Text(
-                  "ADD TO CARD",
-                  style: TextStyle(fontSize: 10),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.all(kDefaultPadding),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: Colors.grey.shade400)),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(books.image),
+                SizedBox(
+                  height: 2,
                 ),
-                onPressed: () {})
-          ],
-        ),
-      ))
-    ]));
+                Text(books.title),
+                SizedBox(
+                  height: 2,
+                ),
+                Text("by ${books.author}"),
+                SizedBox(
+                  height: 2,
+                ),
+                Text("Rs. ${books.price}"),
+                SizedBox(
+                  height: 2,
+                ),
+                Row(children: [
+                  RaisedButton(
+                      color: Colors.orangeAccent,
+                      child: Text(
+                        "ADD TO CARD",
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                      onPressed: () {}),
+                  SizedBox(
+                    width: 1,
+                  ),
+                  Image.asset(
+                    "assets/images/wishlist.png",
+                    width: 30,
+                    height: 30,
+                  )
+                ])
+              ],
+            ),
+          ))
+        ]));
   }
 }
