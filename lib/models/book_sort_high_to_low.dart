f622e2f26944241f6450b791f035f3f11fc1be90
Shradha1824 <shradhabj1824@gmail.com>
2021-09-06 18:20:55 -1200

    List of books created high to low relevance

---
+++ lib/models/book_sort_high_to_low.dart
@@ -0,0 +1,98 @@
+class BookHighToLow {
+  final String image, title, author;
+  final String price;
+
+  BookHighToLow({
+    required this.image,
+    required this.title,
+    required this.author,
+    required this.price,
+  });
+}
+
+List<BookHighToLow> bookshightolow = [
+  BookHighToLow(
+      image: "assets/images/book18.png",
+      title: "Building Planning",
+      author: "S. S. Bhavikatti",
+      price: "1400"),
+  BookHighToLow(
+      image: "assets/images/book20.png",
+      title: "Civil Engineering",
+      author: "S. P. Gupta",
+      price: "1200"),
+  BookHighToLow(
+      image: "assets/images/book21.png",
+      title: "Surveying",
+      author: "S. K. Duggal",
+      price: "1000"),
+  BookHighToLow(
+      image: "assets/images/book14.png",
+      title: "How to Read a Book",
+      author: "Nortimer J. Adler",
+      price: "900"),
+  BookHighToLow(
+      image: "assets/images/book15.png",
+      title: "Winning Sachin",
+      author: "Devendra P.",
+      price: "850"),
+  BookHighToLow(
+      image: "assets/images/book12.png",
+      title: "Oxfort Avanced",
+      author: "Ajit j.",
+      price: "800"),
+  BookHighToLow(
+      image: "assets/images/book9.png",
+      title: "Think like a Rocket",
+      author: "Panik H.",
+      price: "800"),
+  BookHighToLow(
+      image: "assets/images/book11.png",
+      title: "Winning like Sourav",
+      author: "Abhirup B.",
+      price: "750"),
+  BookHighToLow(
+      image: "assets/images/book13.png",
+      title: "Tip Of the Iceberg",
+      author: "Suveen Sinha",
+      price: "670"),
+  BookHighToLow(
+      image: "assets/images/book10.png",
+      title: "How to Speak",
+      author: "Ron Malhotra",
+      price: "600"),
+  BookHighToLow(
+    image: "assets/images/book7.png",
+    title: "Success Story",
+    author: "Vijay K.",
+    price: "600",
+  ),
+  BookHighToLow(
+    image: "assets/images/book6.png",
+    title: "Richest Man",
+    author: "George S Clason",
+    price: "600",
+  ),
+  BookHighToLow(
+      image: "assets/images/book8.png",
+      title: "Chinese Communist",
+      author: "Handry",
+      price: "500"),
+  BookHighToLow(
+    image: "assets/images/book5.png",
+    title: "Just like You",
+    author: "Nick Hornby",
+    price: "500",
+  ),
+  BookHighToLow(
+    image: "assets/images/book4.png",
+    title: "Rising heat",
+    author: "Perumal",
+    price: "400",
+  ),
+  BookHighToLow(
+      image: "assets/images/book3.png",
+      title: "Stephen King",
+      author: "Rumaysa Ahmed",
+      price: "250"),
+];
